6167ac5e027210ad35f35c711e44d35a5d875d19
JimmyT <uv911@yahoo.com>
2017-10-02 20:30:20 -0600

    Try at Tyler's challenge

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,11 @@
 module.exports = function(johnny5) {
-  johnny5.hear(/Hello!/, function(res) {
+
+  johnny5.hear1(/Hello!/, function(res) {
     return res.send("Hi Johnny5 is there!");
+  });
+
+  johnny5.hear(/please welcome/, function(res) {
+    var after = res.match[0].substring(14, 1000);
+    return res.send("Welcome " + after + ". Have a glorious day");
   });
 }
