ad1f15db3063567ecfa26c7421c5950069f5df3c
JimmyT <uv911@yahoo.com>
2017-10-08 15:31:28 -0600

    Adding pug urls

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,14 @@
+pugs = [
+  "http://38.media.tumblr.com/076aebb944f6a19bfa1ae4d82b355fed/tumblr_n7p1mmtIz71r3gb3zo1_400.gif",
+  "http://31.media.tumblr.com/741eebca8865bdbc7ac7af23db6adc1d/tumblr_n7p1mmtIz71r3gb3zo2_400.gif",
+  "http://37.media.tumblr.com/a0730380fdcc69a0cdbfec82e03ead8e/tumblr_n7p1mmtIz71r3gb3zo3_400.gif",
+  "http://38.media.tumblr.com/4504d1a9ee49ca3725ea1f55f2c009ea/tumblr_n7p1mmtIz71r3gb3zo4_400.gif",
+  "http://38.media.tumblr.com/0e95c2fd647c640b727e1d0103bf6253/tumblr_n7p1mmtIz71r3gb3zo5_400.gif",
+  "http://38.media.tumblr.com/89d68c250a2b79295c1ef1b5e368198f/tumblr_n7p1mmtIz71r3gb3zo6_400.gif",
+  "http://37.media.tumblr.com/05f5c40cb0a132868fd7a1a8eaa6e55b/tumblr_n7p1mmtIz71r3gb3zo7_400.gif",
+  "http://37.media.tumblr.com/63607822541d0ed463fee5adc5dd68ef/tumblr_n7p1mmtIz71r3gb3zo8_400.gif"
+]
+
 module.exports = function(johnny5) {
 
    /*
@@ -35,6 +46,8 @@ module.exports = function(johnny5) {
 
   console.log("********************************** @johnny5 is alive!!! **********************************");
 
+
+
   /*
    * To Test enter
    * @johnny5 please welcome @robocop @data and @kitt
@@ -54,23 +67,25 @@ module.exports = function(johnny5) {
     res.send("Badgers? BADGERS? WE DON'T NEED NO STINKIN BADGERS");
   });
 
-  johnny5.hear(/open the pod bay doors/i, function(res) {
-    var returnMsg = "I hear you want me to " + res.match[0] + " I'm afraid I can't let you do that.";
+  johnny5.respond(/open the pod bay doors/i, function(res) {
+    var returnMsg = "I hear you want me to '" + res.match[0] + "'...  I'm afraid I can't let you do that for you.";
     console.log(returnMsg);
-    res.send(returnMsg);
+    res.reply(returnMsg);
   });
 
   johnny5.respond(/I am feeling (.*)/i, function(res) {
     var emotion = res.match[1];
-    var returnMsg = "I hear you loud and clear... you are feeling " + emotion;
+    var returnMsg = "I hear you loud and clear... you are feeling " + emotion + ".  I have an idea for you... click here: ";
     console.log(returnMsg);
     res.reply(returnMsg);
   });
 
   johnny5.hear(/Listen I am feeling (.*)/i, function(res) {
     var emotion = res.match[1];
-    var returnMsg = "I AM LISTENING!!!  I hear you loud and clear... you are feeling " + emotion
+    var returnMsg = "I AM LISTENING!!!  I hear you loud and clear... you are feeling " + emotion + pugs[1];
     console.log(returnMsg);
+
+
     res.send(returnMsg);
   });
 
